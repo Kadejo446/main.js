1d1e16c6cfdd5e2cb1f5ce4fb00eca6799a0fd21
zainab <boluzainab3@gmail.com>
2024-11-16 08:03:50 -0800

    main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+console.log("hello world")
+var presido ="T-Pain"
+console.log(presido)
+console.log("T-Pain")
+var fullName =("Kadejo Zainb")
+var phoneNumber =8058911002
+
+
+console.log(typeof(fullName))
+console.log(typeof(phoneNumber))
+var classMate =["mubarakah","sofiyah","zainab","alim","adura"]
+console.log(classMate)
+var person ={
+    name:"tinubu",
+   job:"Tech Instructor",
+   hobbies:["jumping","cooking"],
+   phone:"8058911002",
+   debt:"null",
+
+}
+var faisol = 1
+var alim = 0
+console.log(faisol/alim)
+console.log(faisol/alim)
+console.log(faisol/alim)
+var age=17
+
+if (age >=1 && age<17){
+    console.log("child")
+} else if (age >=18 && age<35){
+    console.log("youth")
+}else{
+    console.log
+}
+
